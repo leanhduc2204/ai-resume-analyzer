0c7c7a971f89c5eb6b57185f8499ad59c42fdef0
Dom Le <57629315+leanhduc2204@users.noreply.github.com>
2026-06-08 21:13:23 +0700

    ui: homepage, navbar & resume card

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,39 @@
+import type { FC } from "react";
+import { Link } from "react-router";
+import ScoreCircle from "./ScoreCircle";
+
+const ResumeCard: FC<{ resume: Resume }> = ({
+  resume: { id, companyName, jobTitle, feedback, imagePath },
+}) => {
+  return (
+    <Link
+      to={`/resume/${id}`}
+      className="resume-card animate-in fade-in duration-1000"
+    >
+      <div className="resume-card-header">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-black! font-bold wrap-break-word">
+            {companyName}
+          </h2>
+          <h3 className="text-lg wrap-break-word text-gray-500">{jobTitle}</h3>
+        </div>
+
+        <div className="shrink-0">
+          <ScoreCircle score={feedback.overallScore} />
+        </div>
+      </div>
+
+      <div className="gradient-border animate-in fade-in duration-1000">
+        <div className="w-full h-full">
+          <img
+            src={imagePath}
+            alt="resume"
+            className="w-full h-87.5 sm:h-50 object-cover object-top"
+          />
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ResumeCard;
